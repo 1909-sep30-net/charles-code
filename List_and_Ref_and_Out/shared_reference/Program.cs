57b37380f1a52d0ed30c1414995a4317d93841fe
falchion-gpx <charles.g.allensworth@gmail.com>
2019-10-08 21:05:03 -0500

    study in reference variables

---
+++ List_and_Ref_and_Out/shared_reference/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shared_reference
+{
+    public class sampleClass
+    {
+        public int field1;
+        public double field2;
+        public string field3;
+
+        public bool field4;
+
+        public char field5;
+
+        public sampleClass(int f1, double f2, string f3, bool f4, char f5)
+        {
+            this.field1 = f1;
+            this.field2 = f2;
+            this.field3 = f3;
+            this.field4 = f4;
+            this.field5 = f5;
+        }
+
+        public string GetPrintable()
+        {
+            return ( field1.ToString() + " " + field2.ToString() + " " + field3 + " "  + field4.ToString() + " " + field5.ToString() );
+        }
+    } 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing shared references between two lists.");
+
+            //create an empty list
+            List<sampleClass> list1 = new List<sampleClass>();
+
+            //create some class instances
+            sampleClass object1 = new sampleClass(42 , 256.00, "DARKSTAR", true, 'X');
+            sampleClass object2 = new sampleClass(512 , 15.00, "NEUTRONSTAR", false, 'N');
+            sampleClass object3 = new sampleClass(92169 , 4509.00, "VERDANTSTAR", true, 'L');
+
+            //try as a quick add...
+            list1.Add(object1);
+            list1.Add(object2);
+            list1.Add(object3);
+
+            //confirm output
+
+            Console.Out.WriteLine( list1[0].GetPrintable() );
+            Console.Out.WriteLine( list1[1].GetPrintable() );
+            Console.Out.WriteLine( list1[2].GetPrintable() );
+            
+            doHR();
+
+            //works, so ... lets see if we can edit from outside the list.
+
+            object1.field3 = "BLACK HOLE";
+
+            //both refer to the same object????
+            Console.Out.WriteLine( list1[0].GetPrintable() );
+            doHR();
+            //Oh yes, yes, they do....
+            
+            //so lets see if 2 lists will share.
+
+            //create another empty list of the same type.
+            //quickly add the 3 objects to the second list.
+            List<sampleClass> list2 = new List<sampleClass>() {object3, object2, object1};
+
+            //confirm the order...
+            Console.Out.WriteLine( list2[0].GetPrintable() );
+            Console.Out.WriteLine( list2[1].GetPrintable() );
+            Console.Out.WriteLine( list2[2].GetPrintable() );
+            doHR();
+
+            //Now to modify each and every object, we won't use ref here, but will do it manually.
+            object1.field1 = 234;
+            object1.field2 = 324.056;
+            object1.field3 = "SINGULARITY";
+            object1.field4 = true;
+            object1.field5 = '•';
+
+            //confirm changes to object, which should be reflected in both lists....
+            //object1
+            Console.Out.WriteLine( object1.GetPrintable() );
+            doHR();
+
+            //confirm reference to object1
+            Console.Out.WriteLine( list1[0].GetPrintable() );
+            doHR();
+            //conform yet another reference to object1, also...
+            Console.Out.WriteLine( list2[2].GetPrintable() );
+            doHR();
+
+            //in conclusion, objects put into a list are PASSED BY REFERENCE.
+            //           :)
+
+            //now lets use a method to modify the referenced-object.... in a list.
+            //note how we are using a reference
+            modifyObject(ref object1 );
+
+            //confirm all changes...
+            Console.Out.WriteLine( list1[0].GetPrintable() );
+            doHR();
+            //conform yet another reference to object1, also...
+            Console.Out.WriteLine( list2[2].GetPrintable() );
+            doHR();
+
+            //see if we even need to bother with ref
+
+            //new object
+            sampleClass object4 = new sampleClass( 35, (double) Math.PI, "🏝️", false, 'D' );
+            Console.Out.WriteLine( object4.GetPrintable() );
+            doHR();
+
+            //add it to the lists with the no-ref method
+            addToBothLists(list1, list2, object4);
+            //confirm all changes...
+            Console.Out.WriteLine( list1[0].GetPrintable() );
+            Console.Out.WriteLine( list1[1].GetPrintable() );
+            Console.Out.WriteLine( list1[2].GetPrintable() );
+            Console.Out.WriteLine( list1[3].GetPrintable() );
+            doHR();
+
+            Console.Out.WriteLine( list2[0].GetPrintable() );
+            Console.Out.WriteLine( list2[1].GetPrintable() );
+            Console.Out.WriteLine( list2[2].GetPrintable() );
+            Console.Out.WriteLine( list2[3].GetPrintable() );
+            doHR();
+
+            //see if they are unique objects, or also references.
+            modifyObject2(object4);
+            Console.Out.WriteLine( object4.GetPrintable() );
+            doHR();
+
+            //confirm all changes...
+            Console.Out.WriteLine( list1[0].GetPrintable() );
+            Console.Out.WriteLine( list1[1].GetPrintable() );
+            Console.Out.WriteLine( list1[2].GetPrintable() );
+            Console.Out.WriteLine( list1[3].GetPrintable() );
+            doHR();
+
+            Console.Out.WriteLine( list2[0].GetPrintable() );
+            Console.Out.WriteLine( list2[1].GetPrintable() );
+            Console.Out.WriteLine( list2[2].GetPrintable() );
+            Console.Out.WriteLine( list2[3].GetPrintable() );
+            doHR();
+
+            //which works
+            // indicates that ref is not needed at all.... for reference varables
+            // and that no matter what, lists take references to objects and are
+            // shallow copy.
+            //
+            // For reference-type variables,
+            //    if a pass-by-value is desired, 
+            //    a deep-copy method will be required.
+            // 1. create the new object of the same class
+            // 2. copy all the variables from one object to another
+            // 3. put new object into list.
+
+
+
+        }
+
+        public static void doHR()
+        {
+            Console.Out.WriteLine("\n----------------------------\n");
+        }
+
+        //simply converts everything to emogee robot info
+        public static void modifyObject(ref sampleClass obj)
+        {
+            obj.field1 = 129302;
+            obj.field2 = 129302.0;
+            obj.field3 = "HUMAN | 😏🤖 | ROBOT"; //this works, as string decodes Unicode
+            obj.field4 = false;
+            //won't work, sadly, needs more maths before we can get the unicode character as a char.
+            //obj.field5 = 🤖;
+            obj.field5 = 'R';
+
+        }
+
+        public static void modifyObject2(sampleClass obj)
+        {
+            obj.field1 = 9;
+            obj.field2 = 4.0;
+            obj.field3 = "DESERT ISLAND";
+            obj.field4 = false;
+            obj.field5 = 'Q';
+
+        }
+
+        public static void addToBothLists( List<sampleClass> listun, List<sampleClass> listdeux, sampleClass obj)
+        {
+            listun.Add(obj);
+            listdeux.Add(obj);
+
+        }
+
+    }
+}
--- SQL Queries/SQLQuery1.sql
+++ SQL Queries/SQLQuery1.sql
@@ -1,5 +1,7 @@
 -- basic exercises (Chinook database)
 
+-- Business requirements documents will require good sense in order to interpret the request vs. actual.
+
 -- 1. list all customers (full names, customer ID, and country) who are not in the US
 
 SELECT FirstName + ' ' + LastName AS [FullName], CustomerID, Country
@@ -18,10 +20,24 @@ SELECT *
 FROM Employee
 WHERE Title = 'Sales Support Agent';
 
+--this works too
+-- From Mr. Nick
+SELECT *
+FROM Employee
+Where Title LIKE '%Sales%Agen_%'; -- % is the wildcard.
+--pattern matching with the LIKE operator
+-- % - 0 to many of any character
+-- [abc] - one of a, b, or c
+-- _ = 1 of any character
+
 -- 4. show a list of all countries in billing addresses on invoices.
 
+--distinct is from the Select...
 SELECT DISTINCT BillingCountry
 FROM Invoice
+
+--Select Distinct means, that after you get all the results rows... remove all the duplicate rows.
+-- can be slower on the server, but is faster to transmit (trimmed data)
 
 
 -- 5. how many invoices were there in 2009, and what was the sales total for that year?
@@ -30,6 +46,69 @@ FROM Invoice
 SELECT COUNT (*) [InvCount], SUM(Total)[2009Sales] 
 FROM Invoice
 WHERE YEAR(InvoiceDate) = 2009;
+
+--KC's Answer
+SELECT SUM(Total) AS TOtalAmount, COUNT(InvoiceID) AS InvoicesIn2019
+FROM Invoice WHERE InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31'
+--also
+--WHERE YEAR(InvoiceDate)=2009
+
+--another's answer
+--Extra Challenge Answer
+SELECT DISTINCT YEAR(InvoiceDate) AS Year, SUM(Total) AS TOtalAmount, COUNT(InvoiceID) AS InvoicesIn2019
+FROM Invoice
+GROUP BY YEAR(InvoiceDate);
+
+
+-- So Far
+--Select statements have several "clauses"
+
+-- Select clause specifies what columns to have in the result set
+-- FROM clause specifies what table to query
+-- WHERE clause defines a condition , filtering out rows that don't match
+-- GROUP BY clause aggregates rows together based on common information in a column
+-- Having clause defines a condition for filtering, but AFTER the group-by
+-- ORDER BY clause specifies the sort-order of the result set.  Order by default is undefined.
+--          best to order the data as a best practice.
+
+SELECT COUNT(*)  -- <-- aggregate function  to get number of rows in a DB table
+FROM Customer;
+
+--find out about any duplicate first names across customers!
+
+--count people who have the same first name.
+SELECT FirstName, Count(*) [DupNameCount]
+FROM Customer
+GROUP BY FirstName;
+
+--same first and last name?  Probably not.
+SELECT FirstName, LastName, Count(*) [DupNameCount]
+FROM Customer
+GROUP BY FirstName, LastName DESC;--not putting LastName here creates problems... first set is often last set.
+
+--aggregate function takes in multiple rows (values) and returns one value
+
+
+-- Don't care about the unique names?
+SELECT FirstName, Count(*) AS DuplicateCount
+FROM Customer
+--WHERE COUNT(*) > 1  <-- not posssible, WHERE runs before GROUP BY
+GROUP BY FirstName
+HAVING COUNT(*) > 1  -- <-- having is added onto the end
+ORDER BY FirstName;  -- <--and storted!
+
+--- More on Select
+
+-- logical order of operations as a whole in Select statements
+
+--1. SELECT
+--2. FROM
+--3. WHERE
+--4. GROUP BY
+--5. HAVING
+--6. ORDER BY
+
+
 
 -- 6. how many line items were there for invoice #37?
 
@@ -51,3 +130,9 @@ SELECT BillingCountry, SUM(Total) [TotalSales]
 FROM Invoice
 GROUP BY BillingCountry
 ORDER BY TotalSales DESC; 
+
+--Another Way by Mr. Nick
+SELECT BillingCountry, SUM(Total)
+FROM Invoice
+GROUP BY BillingCountry
+ORDER BY SUM(Total) DESC, BillingCountry;  -- <-- possible to have secondary sort criteria.
