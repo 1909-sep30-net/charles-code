82f38a1e5efbf27aa943991b9cb1a3e4be938e77
falchion-gpx <charles.g.allensworth@gmail.com>
2019-10-15 13:21:11 -0500

    HTML Asssignments

--- HelloRevature/Program.cs
+++ HelloRevature/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HelloRevature
 {
@@ -6,7 +7,46 @@ namespace HelloRevature
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Revature!");
+
+            
+            int mills = Math.Abs( (int) (( DateTimeOffset.Now.ToUnixTimeMilliseconds() % 256) * Math.PI) ) ;
+            Random rand = new Random(mills);
+            int random = rand.Next(1, 16  );
+
+            Console.WriteLine($"TIme is now { mills} and Random is now : {random }");
+
+            char[] hexVal = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+            string stringID = "";
+
+            IDictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(10,"A");
+            dict.Add(11,"B");
+            dict.Add(12,"C");
+            dict.Add(13,"D");
+            dict.Add(14,"E");
+            dict.Add(15,"F");
+
+            for(int i = 0 ; i <= 15 ; i++)
+            {
+                random = rand.Next(0, 15  );
+                if(random <= 9)
+                {
+                    stringID += random.ToString();
+                }
+                else
+                {
+                    stringID += dict[random];
+                }
+
+            }
+
+            Console.WriteLine($"The Code is : { stringID }...");
+                
+            Console.WriteLine( DateTime.Now.ToString() ) ;
+            
+
+
         }
     }
 }
